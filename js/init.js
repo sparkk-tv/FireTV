3cdd8a8411de57a2dbcad9f9c0aeb5764347d984
Aaron Bielert <services@sparkktv.com>
2019-01-31 08:37:31 -0500

    Update init.js

--- js/init.js
+++ js/init.js
@@ -18,18 +18,143 @@
             },
             {
                 type: "playlist",
-                id: ["PL4A79YBwLx9cijP5V4FnYL51Ys5jUy__s"],
-                title: "Above The Fold"
-            },
-            {
-                type: "playlist",
                 id: ["PL4A79YBwLx9dftxTfDKQkFDUI1Z1kI01A"],
                 title: "Aidan 5"
             },
             {
                 type: "playlist",
                 id: ["PL4A79YBwLx9cbPFzIqnzlfy3x7Zya-70I"],
-                title: "Alexander Reese"
+                title: "All About Manos"
+            },
+            {
+                type: "playlist",
+                id: ["PL4A79YBwLx9cijP5V4FnYL51Ys5jUy__s"],
+                title: "All That Shabaz"
+            },
+            {
+                type: "playlist",
+                id: ["PL4A79YBwLx9cijP5V4FnYL51Ys5jUy__s"],
+                title: "Among Friends"
+            },
+            {
+                type: "playlist",
+                id: ["PL4A79YBwLx9cijP5V4FnYL51Ys5jUy__s"],
+                title: "Aurora: Chronicles Of A Hero"
+            },
+            {
+                type: "playlist",
+                id: ["PL4A79YBwLx9cijP5V4FnYL51Ys5jUy__s"],
+                title: "Battle Jar"
+            },
+            {
+                type: "playlist",
+                id: ["PL4A79YBwLx9cijP5V4FnYL51Ys5jUy__s"],
+                title: "Best Friend Dad"
+            },
+            {
+                type: "playlist",
+                id: ["PL4A79YBwLx9cijP5V4FnYL51Ys5jUy__s"],
+                title: "Body Jumpers"
+            },
+            {
+                type: "playlist",
+                id: ["PL4A79YBwLx9cijP5V4FnYL51Ys5jUy__s"],
+                title: "Body Jumpers: Resurrection"
+            },
+            {
+                type: "playlist",
+                id: ["PL4A79YBwLx9cijP5V4FnYL51Ys5jUy__s"],
+                title: "Boost"
+            },
+            {
+                type: "playlist",
+                id: ["PL4A79YBwLx9cijP5V4FnYL51Ys5jUy__s"],
+                title: "BrothAhood"
+            },
+            {
+                type: "playlist",
+                id: ["PL4A79YBwLx9cijP5V4FnYL51Ys5jUy__s"],
+                title: "BTS On The Road"
+            },
+            {
+                type: "playlist",
+                id: ["PL4A79YBwLx9cijP5V4FnYL51Ys5jUy__s"],
+                title: "Caledonia"
+            },
+            {
+                type: "playlist",
+                id: ["PL4A79YBwLx9cijP5V4FnYL51Ys5jUy__s"],
+                title: "Cheerleader Karate School"
+            },
+            {
+                type: "playlist",
+                id: ["PL4A79YBwLx9cijP5V4FnYL51Ys5jUy__s"],
+                title: "Clean Livin"
+            },
+            {
+                type: "playlist",
+                id: ["PL4A79YBwLx9cijP5V4FnYL51Ys5jUy__s"],
+                title: "Color Blind"
+            },
+            {
+                type: "playlist",
+                id: ["PL4A79YBwLx9cijP5V4FnYL51Ys5jUy__s"],
+                title: "Consequences"
+            },
+            {
+                type: "playlist",
+                id: ["PL4A79YBwLx9cijP5V4FnYL51Ys5jUy__s"],
+                title: "Davey's Diary"
+            },
+            {
+                type: "playlist",
+                id: ["PL4A79YBwLx9cijP5V4FnYL51Ys5jUy__s"],
+                title: "Dawn"
+            },
+            {
+                type: "playlist",
+                id: ["PL4A79YBwLx9cijP5V4FnYL51Ys5jUy__s"],
+                title: "Death's Door"
+            },
+            {
+                type: "playlist",
+                id: ["PL4A79YBwLx9cijP5V4FnYL51Ys5jUy__s"],
+                title: "Downtown Girls"
+            },
+            {
+                type: "playlist",
+                id: ["PL4A79YBwLx9cijP5V4FnYL51Ys5jUy__s"],
+                title: "Drifter"
+            },
+            {
+                type: "playlist",
+                id: ["PL4A79YBwLx9cijP5V4FnYL51Ys5jUy__s"],
+                title: "Eris"
+            },
+            {
+                type: "playlist",
+                id: ["PL4A79YBwLx9cijP5V4FnYL51Ys5jUy__s"],
+                title: "Ex-Factor"
+            },
+            {
+                type: "playlist",
+                id: ["PL4A79YBwLx9cijP5V4FnYL51Ys5jUy__s"],
+                title: "Falling Apart"
+            },
+            {
+                type: "playlist",
+                id: ["PL4A79YBwLx9cijP5V4FnYL51Ys5jUy__s"],
+                title: "Flicks"
+            },
+            {
+                type: "playlist",
+                id: ["PL4A79YBwLx9cijP5V4FnYL51Ys5jUy__s"],
+                title: "Frontier Guard"
+            },
+            {
+                type: "playlist",
+                id: ["PL4A79YBwLx9cijP5V4FnYL51Ys5jUy__s"],
+                title: "Geez Louise"
             },
             {
                 type: "playlist",
